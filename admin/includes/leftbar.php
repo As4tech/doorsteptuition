3dc9910d13eaac7ebb0076c341e18755f3ea0f0c
abdulwadud98 <94097298+abdulwadud98@users.noreply.github.com>
2021-11-11 13:26:47 +0000

    Door step tuition project

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,25 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			
+			<li><a href="profile.php"><i class="fa fa-user"></i> &nbsp;Profile</a>
+			</li>
+			<li><a href="userlist.php"><i class="fa fa-users"></i> Tutors</a>
+			</li>
+			<li><a href="registered_rooms.php"><i class="fa fa-users"></i>Students</a>
+			</li>
+
+			<li><a href="feedback.php"><i class="fa fa-envelope"></i> &nbsp;Feedback</a>
+			</li>
+			<li><a href="notification.php"><i class="fa fa-bell"></i> &nbsp;Notification <sup style="color:red">*</sup></a>
+			</li>
+			<li><a href="deleteduser.php"><i class="fa fa-user-times"></i> &nbsp;Deleted Users</a>
+			</li>
+			<li><a href="download.php"><i class="fa fa-download"></i> &nbsp;Export Tutors</a>
+			</li>
+			</ul>
+		</nav>
+
+		
